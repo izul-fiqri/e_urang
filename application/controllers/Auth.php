90ef0e531036bfc2abed577debfd9306e889b918
izul-fiqri <izulfiqri98@gmail.comm>
2022-10-03 04:02:50 +0700

    menambahkan file codeigniter

---
+++ application/controllers/Auth.php
@@ -0,0 +1,45 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+  public function login()
+  {
+    // check_already_login();
+    $this->load->view('login');
+  }
+
+  public function process()
+  {
+    $post = $this->input->post(null, TRUE);
+    if (isset($post['login'])) {
+      $this->load->model('mUser');
+      $query = $this->mUser->login($post);
+      if ($query->num_rows() > 0) {
+        $row = $query->row();
+        $params = array(
+          'user_id' => $row->user_id,
+          'level' => $row->level
+        );
+
+        $this->session->set_userdata($params);
+
+        echo "<script> 
+        alert('Selamat, login berhasil');
+        window.location='" . site_url('dashboard') . "';
+        </script>";
+      } else {
+        echo "<script> alert('login gagal, username/password salah');
+        window.location='" . site_url('auth/login') . "';
+        </script>";
+      }
+    }
+  }
+
+  public function logout()
+  {
+    $params = array('user_id', 'level');
+    $this->session->set_userdata($params);
+    redirect('auth/login');
+  }
+}
